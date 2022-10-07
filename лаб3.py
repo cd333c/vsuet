5db1c06357c54f19b549ca53a8ae043c5261bb8f
cd333c <114305977+cd333c@users.noreply.github.com>
2022-10-07 16:11:08 -0400

    Add files via upload

---
+++ лаб3.py
@@ -0,0 +1,9 @@
+a=float(input())
+b=float(input())
+c=float(input())
+if a in range(1,3) or a==3 :
+    print(a)
+if b in range(1,3) or b==3:
+    print(b)
+if c in range(1,3) or c==3:
+    print(c)
